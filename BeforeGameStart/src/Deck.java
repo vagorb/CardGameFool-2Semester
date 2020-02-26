55f0f870cda15e48b27ce700d6aea9246add6f0d
vagorb <vagorb@ttu.ee>
2020-02-26 19:52:09 +0200

    Example with right pom configuration

---
+++ BeforeGameStart/src/Deck.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Deck {
+    ArrayList<String> suits = new ArrayList<>(Arrays.asList("SPADES", "DIAMONDS", "HEARTS", "CLUBS"));
+    ArrayList<String> values = new ArrayList<>(Arrays.asList("6", "7", "8", "9", "10", "JACK", "'QUEEN", "KING", "ACE"));
+    ArrayList<Card> deck;
+    public Deck(){
+        this.deck = createDeck();
+    }
+
+    public ArrayList<Card> createDeck() {
+        ArrayList<Card> deck = new ArrayList<>();
+        for (String value: values) {
+            for (String suit : suits) {
+                if (value.equals("10")){
+                    Card card = new Card(suit, value, null, Card.Visibility.NOONE, "0" + suit.substring(0,1));
+                    deck.add(card);
+                } else {
+                    Card card = new Card(suit, value, null, Card.Visibility.NOONE, value.substring(0, 1)
+                            + suit.substring(0, 1));
+                    deck.add(card);
+                }
+            }
+        }
+        Collections.shuffle(deck);
+        return deck;
+    }
+
+
+}
