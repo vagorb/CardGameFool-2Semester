66709ca559e51c3345e1bd94fe6227a335ff06af
Dima Voronoi <dvoron@ttu.ee>
2020-03-04 21:35:21 +0200

    Test card push

---
+++ cardClassCombined/src/Table.java
@@ -0,0 +1,102 @@
+import java.lang.management.PlatformLoggingMXBean;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Table {
+
+    private List<Card> deckForThisGame;
+    private List<Player> listOfPlayers = new ArrayList<>();
+    private List<Card> pileOfCardsForThisGame;
+
+    public Table(Player player1, Player player2) {
+        deckForThisGame = Deck.createDeck();
+        Pile pile = new Pile();
+        pileOfCardsForThisGame = pile.createPile();
+        listOfPlayers.add(player1);
+        listOfPlayers.add(player2);
+        System.out.println(deckForThisGame);
+    }
+
+    public Table(Player player1, Player player2, Player player3) {
+        deckForThisGame = Deck.createDeck();
+        Pile pile = new Pile();
+        pileOfCardsForThisGame = pile.createPile();
+        listOfPlayers.add(player1);
+        listOfPlayers.add(player2);
+        listOfPlayers.add(player3);
+        System.out.println(deckForThisGame);
+    }
+
+    public Table(Player player1, Player player2, Player player3, Player player4) {
+        deckForThisGame = Deck.createDeck();
+        Pile pile = new Pile();
+        pileOfCardsForThisGame = pile.createPile();
+        listOfPlayers.add(player1);
+        listOfPlayers.add(player2);
+        listOfPlayers.add(player3);
+        listOfPlayers.add(player4);
+        System.out.println(deckForThisGame);
+    }
+
+    public List<Player> getPlayers() {
+        return listOfPlayers;
+    }
+
+    public List<Card> getGameDeck() {
+        return deckForThisGame;
+    }
+
+    public List<Card> getPile() {
+        return pileOfCardsForThisGame;
+    }
+
+    public void addToPile(Card card) {
+        pileOfCardsForThisGame.add(card);
+        deckForThisGame.remove(card);
+    }
+
+
+
+
+
+
+    // not sure about this either right now
+//    private Set<Card> defenseAndAttackCards = new HashSet<>();
+    // Okey i am retarded
+//    private Set<Card> pileOfCards = new HashSet<>();
+    // We should probably have a Player class after all
+//    private Set<Player> playersAtThisTable = new HashSet<>();
+    // Table class description
+    // Class that holds information about everything on the table
+    // Deck info
+    // Cards that are put against the player
+    // Cards we defend with
+    // Pile ( cards that were discarded (Successful defense against attacking player(es)
+    // Info about players sitting(playing) at this table ( NOT SURE IF THIS CLASS SHOULD DO IT)
+
+    // We should probably use table as a getter/setter for info
+
+//    public List<Card> getPileOfCards() {
+//        return Pile.getPileOfCards;
+//    }
+//
+//
+//    public Set<Players> getPlayersAtThisTable() {
+//        return playersAtThisTable;
+//    }
+//
+//    public void setPlayersAtThisTable(Player PlayerName) {
+//        playersAtThisTable.add(PlayerName);
+//    }
+
+
+
+
+//    public void addGameDeckToTable() {
+//
+//        deckForThisGame.add(Deck.createDeck())
+//    }
+
+}
