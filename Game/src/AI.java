842b86910a4c207ae4f187e1aeb4413c03668dbc
vagorb <vagorb@ttu.ee>
2020-04-15 21:11:21 +0300

    #27 commit with same aim

--- Game/src/AI.java
+++ Game/src/AI.java
@@ -190,6 +190,6 @@ public class AI {
             }
         }
 
-        
+
     }
 }
---
+++ Server/src/ServerTCP.java
@@ -0,0 +1,4 @@
+public class ServerTCP {
+    private final String ServerIp = "193.40.255.14";
+    public int port = 5201;
+}
